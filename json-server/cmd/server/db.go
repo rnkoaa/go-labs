a9291d45c54b29d8eff947c96b548b0ddb360779
Richard.AmoakoAgyei <richard.amoakoagyei@target.com>
2020-04-13 15:38:42 -0500

    json server

---
+++ json-server/cmd/server/db.go
@@ -0,0 +1,9 @@
+package main
+
+// import "github.com/rnkoaa/go-labs/json-server/pkg/domain"
+// "github.com/rnkoaa/go-labs/json-server/pkg/domain"
+
+// Database -
+type Database struct {
+	Posts []*Post `json:"posts"`
+}
