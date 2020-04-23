d580a9358f5f267bf6c2c65b15e9a5e99efa3dd2
Richard Agyei <richard.agyei@gmail.com>
2020-04-23 00:06:53 -0500

    Test infinite loop

--- json-placeholder/cmd/json-placeholder/main.go
+++ json-placeholder/cmd/json-placeholder/main.go
@@ -19,9 +19,25 @@ func main() {
 	defer cancel()
 	client := jsonplaceholder.NewClient(nil)
 	client.SetBaseURL(baseURL)
-	user, _, err := client.User.Get(ctx, 1)
-	if err != nil {
-		fmt.Printf("error requesting post: %v\n", err)
+	runMultipleTimes(ctx, client, 1)
+}
+
+func runMultipleTimes(ctx context.Context, client *jsonplaceholder.Client, userId int) {
+	totalCount := 10
+	count := 0
+	for {
+		fmt.Printf("Count -> %d\n", count)
+		if count == totalCount {
+			fmt.Println("We have reached max count, returning")
+			break
+		}
+		count++
+		user, _, err := client.User.Get(ctx, 1)
+		if err != nil {
+			fmt.Printf("error requesting post: %v\n", err)
+		}
+		fmt.Println(jsonplaceholder.Stringify(user))
+		fmt.Println("=============================================================================")
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Println(jsonplaceholder.Stringify(user))
 }
--- json-placeholder/pkg/jsonplaceholder/user.go
+++ json-placeholder/pkg/jsonplaceholder/user.go
@@ -6,46 +6,13 @@ import (
 	"net/http"
 )
 
-/*
-{
-      "userId": 1,
-      "id": 20,
-      "title": "ullam nobis libero sapiente ad optio sint",
-      "completed": true
-	}
-*/
+// Todo represents a todo json object response
 type Todo struct {
 	UserID    int    `json:"userId"`
 	ID        int    `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
-
-/*
-{
-  "id": 1,
-  "name": "Leanne Graham",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
-  }
-}
-*/
 
 // User - object
 type User struct {
